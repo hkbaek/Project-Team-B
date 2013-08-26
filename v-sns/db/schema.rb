d2a54c43e14e960299632d442dd30dfe68d662c0
majestin <leekorea09@gmail.com>
2013-08-26 23:06:53 +0900

    Post 추가
    scaffoldd로 Post 생성 후 일부분 수정 , posts_controller에 filter 적

--- v-sns/db/schema.rb
+++ v-sns/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130826111203) do
+ActiveRecord::Schema.define(version: 20130826130738) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "roles", force: true do |t|
     t.string   "name"
