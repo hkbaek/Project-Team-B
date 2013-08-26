8dff9c8fdc4f857d5fa0ce6cda5a03212d036cf6
Jaejin Kim <nije1983@gmail.com>
2013-08-26 17:11:30 +0900

    use devise

--- v-sns/app/models/user.rb
+++ v-sns/app/models/user.rb
@@ -1,41 +1,7 @@
-require 'digest/sha2'
-
 class User < ActiveRecord::Base
-
-	validates :email, :presence => true, :uniqueness => true
-	
-	validates :password, :confirmation => true
-	attr_accessor :password_confirmation
-	attr_reader :password
-	validate :password_must_be_present
-	
-	def User.authenticate(name, password)
-		if user = find_by_name(name)
-			if user.hashed_password == encrypt_password(password, user.salt)
-				user
-			end
-		end
-	end
-
-	def User.encrypt_password(password, salt)
-		Digest::SHA2.hexdigest(password + salt)
-	end
-	
-	# 'password' is a virtual attribute
-	def password=(password)
-		@password = password
-		if password.present?
-			generate_salt
-			self.hashed_password = self.class.encrypt_password(password, salt)
-		end
-	end
-
-	private
-		def password_must_be_present
-			errors.add(:password, "Missing password" ) unless hashed_password.present?
-		end
-		
-		def generate_salt
-			self.salt = self.object_id.to_s + rand.to_s
-		end
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- v-sns/config/routes.rb
+++ v-sns/config/routes.rb
@@ -1,6 +1,5 @@
 VSns::Application.routes.draw do
-  resources :users
-
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
