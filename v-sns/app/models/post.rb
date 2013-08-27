38810812f7797933a8c6aa7dd71d2c0a36065568
majestin <leekorea09@gmail.com>
2013-08-27 21:40:35 +0900

    Post 부분 UPDATE
    자신이 쓴 Post만 볼 수 있음.

--- v-sns/app/models/post.rb
+++ v-sns/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	# 유저가 지워지면 게시물도 다 같이 지워진다.
   belongs_to :user
 
   mount_uploader :image, ImageUploader  
--- v-sns/app/models/user.rb
+++ v-sns/app/models/user.rb
@@ -2,14 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-	belongs_to :role
-	before_create :set_default_role
-
+  belongs_to :role
+  before_create :set_default_role
+  has_many :posts
+  
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-	private
-	def set_default_role
-		self.role ||= Role.find_by_name('registerd')
-	end
+  private
+  def set_default_role
+  	self.role ||= Role.find_by_name('registerd')
+  end
 end
