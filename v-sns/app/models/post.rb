eaca2e0ad3ddcbc3c51a77558afe148911a99525
doohee <doohee323@gmail.com>
2013-08-28 01:15:45 +0900

    Tag 기능 추

--- v-sns/app/models/post.rb
+++ v-sns/app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
   has_many :comments, :dependent => :destroy
+  has_many :tags, :dependent => :destroy
+
+	accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 
   mount_uploader :image, ImageUploader  
 end
--- v-sns/db/schema.rb
+++ v-sns/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827152410) do
+ActiveRecord::Schema.define(version: 20130827154101) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20130827152410) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["post_id"], name: "index_tags_on_post_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
