ec909b76b04ba5c5e460e942e61b6dd7f49c98d3
Jaejin Kim <nije1983@gmail.com>
2013-08-27 10:47:21 +0900

    added side bar

--- v-sns/config/environments/development.rb
+++ v-sns/config/environments/development.rb
@@ -30,5 +30,5 @@ VSns::Application.configure do
 	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 	config.action_mailer.delivery_method = :smtp
 	# config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025, :domain => "de.chemist.com" }
-	config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", :port => 587, :domain => "de.chemist.com", :user_name => "nije1983@gmail.com", :password => "WOwls*#73" }
+	config.action_mailer.smtp_settings = {:address => "smtp.gmail.com", :port => 587, :domain => "de.chemist.com", :user_name => "dev.study.member@gmail.com", :password => "devstudymember" }
 end
--- v-sns/db/schema.rb
+++ v-sns/db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20130826132432) do
-=======
-ActiveRecord::Schema.define(version: 20130826130738) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -25,7 +22,6 @@ ActiveRecord::Schema.define(version: 20130826130738) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
->>>>>>> original/master
 
   create_table "roles", force: true do |t|
     t.string   "name"
